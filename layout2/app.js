4cddd8b8f686f2a5eeb421c319874af01290dabf
juliakrick <krickunova.iulia@yandex.ru>
2020-11-15 23:05:52 +0300

    layout DecoBlog

---
+++ layout2/app.js
@@ -0,0 +1,17 @@
+'use strict'
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200,
+  gutter: 35
+
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  gutter: 35
+});
+
+
