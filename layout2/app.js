2d1d44795146504b5736a52c7ac1910afc854369
juliakrick <krickunova.iulia@yandex.ru>
2021-02-14 11:40:39 +0300

    html and css

--- layout2/app.js
+++ layout2/app.js
@@ -1,18 +1,39 @@
 'use strict'
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200,
-  gutter: 35
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   columnWidth: 200,
+//   // gutter: 35
 
-});
+// });
 
 // element argument can be a selector string
 //   for an individual element
 var msnry = new Masonry( '.grid', {
-  gutter: 30
+  gutter: 30,
+  horizontalOrder: true
 });
+
+// слайдер
+
+$(document).ready(function(){
+  $('.slider_wrapper').slick({
+    infinite: true,
+    speed: 100,
+    fade: true,
+    cssEase: 'linear',
+    // autoplay: true,
+    prevArrow: '<img class="prev" src="img/top_155.png" alt="1"></img>',
+    nextArrow: '<img class="next" src="img/down_156.png" alt="2"></img>',
+    dots: true,
+    // asNavFor:
+    // dotsClass: 'square',
+  });
+});
+
+
+
 
 
 
